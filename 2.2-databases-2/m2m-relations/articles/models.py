ed6a88a372e61c1b8d2c7744ff963b0670eebcf6
Matthewous <moguch98@mail.ru>
2023-03-06 02:27:18 +0300

    Построена структура БД

--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -11,6 +11,27 @@ class Article(models.Model):
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
+        ordering = ['-published_at']
 
     def __str__(self):
         return self.title
+
+class Tag(models.Model):
+    name = models.CharField(max_length=256, verbose_name='Название')
+    articles = models.ManyToManyField(Article, related_name='tags', through='Scope')
+    
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+class Scope(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
+    tag = models.ForeignKey(Tag, verbose_name='Раздел', on_delete=models.CASCADE, related_name='scopes')
+    is_main = models.BooleanField(default=False, verbose_name='основной')
+
+    class Meta:
+        ordering = ['-is_main', 'tag']
