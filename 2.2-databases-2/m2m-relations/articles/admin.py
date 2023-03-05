1b2647ef3532b46dbb447494003414836bb44799
Matthewous <moguch98@mail.ru>
2023-03-06 02:28:08 +0300

    Доработана админка

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,35 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Tag, Scope
 
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+
+        is_main_num = 0
+        for form in self.forms:
+            if form.cleaned_data.get('is_main')==True:
+                is_main_num += 1
+
+        if is_main_num > 1:   
+            raise ValidationError('Основным может быть только один раздел')
+        if is_main_num < 1:
+            raise ValidationError('Укажите основной раздел')
+
+        return super().clean()  # вызываем базовый код переопределяемого метода
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    formset = ScopeInlineFormset
+    extra = 1
 
 @admin.register(Article)
-class ArticleAdmin(admin.ModelAdmin):
-    pass
+class ObjectAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'published_at']
+    inlines = [ScopeInline]
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
