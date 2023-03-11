272274a2bbe043410b4fcbcdea5e0a3d685c6506
Matthewous <moguch98@mail.ru>
2023-03-12 01:13:26 +0300

    Корректировка ограничений

--- 3.1-drf-intro/smart_home/measurement/models.py
+++ 3.1-drf-intro/smart_home/measurement/models.py
@@ -3,10 +3,9 @@ from django.db import models
 # TODO: опишите модели датчика (Sensor) и измерения (Measurement)
 
 class Sensor(models.Model):
-    name = models.CharField(max=50)
+    name = models.CharField(max_length=50)
     description = models.TextField()
 
 class Measurement(models.Model):
     temperature = models.FloatField()
     date = models.DateTimeField()
-    
