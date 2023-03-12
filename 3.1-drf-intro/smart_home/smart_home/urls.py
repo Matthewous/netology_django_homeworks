60fc9482972581dc2de7c38f56e4fd132812721a
Matthewous <moguch98@mail.ru>
2023-03-12 03:11:13 +0300

    удалены лишние связи

--- 3.1-drf-intro/smart_home/smart_home/urls.py
+++ 3.1-drf-intro/smart_home/smart_home/urls.py
@@ -16,8 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from measurement.models import Sensor_change, Sensor_info
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('measurement.urls')),    
